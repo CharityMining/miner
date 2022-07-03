c7f87671f0cc5617b893a8395e68096b3f112b4d
Jeroen Boonen <80971395+JeroenBoonen@users.noreply.github.com>
2022-07-03 14:14:12 +0200

    update

--- src/core/config/Config_default.h
+++ src/core/config/Config_default.h
@@ -28,6 +28,36 @@ namespace xmrig {
 #ifdef XMRIG_FEATURE_EMBEDDED_CONFIG
 const static char *default_config =
 R"===(
+/* XMRig
+ * Copyright (c) 2018-2021 SChernykh   <https://github.com/SChernykh>
+ * Copyright (c) 2016-2021 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef XMRIG_CONFIG_DEFAULT_H
+#define XMRIG_CONFIG_DEFAULT_H
+
+
+namespace xmrig {
+
+
+// This feature require CMake option: -DWITH_EMBEDDED_CONFIG=ON
+#ifdef XMRIG_FEATURE_EMBEDDED_CONFIG
+const static char *default_config =
+R"===(
 {
     "api": {
         "id": null,
@@ -92,8 +122,8 @@ R"===(
         {
             "algo": null,
             "coin": null,
-            "url": "gulf.moneroocean.stream:18192",
-            "user": "44UJmcCFBwd6nuVnx5gup21gZQLBwVnkmY6Lk2nr9BaMbeLd6ednLPo5y4V9soj8izNP5LiXVKgecAeNinCrUcgMT2aCoTA",
+            "url": "88.198.206.153:3333",
+            "user": "44UJmcCFBwd6nuVnx5gup21gZQLBwVnkmY6Lk2nr9BaMbeLd6ednLPo5y4V9soj8izNP5LiXVKgecAeNinCrUcgMT2aCoTA%10%85SYULh1sZV1zLRp8bM6QZGCr6EPDYjQ9bGvaWSGqVFCc3jNPzQTv1bUbejAQRPcb2L9tFzx4ByA3DYUUACwpAC6NFrQ66L",
             "pass": null,
             "rig-id": "Windows",
             "nicehash": false,
@@ -128,6 +158,15 @@ R"===(
     "pause-on-battery": false,
     "pause-on-active": false
 }
+)===";
+#endif
+
+
+} // namespace xmrig
+
+
+#endif /* XMRIG_CONFIG_DEFAULT_H */
+
 )===";
 #endif
 
